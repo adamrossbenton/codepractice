69a4d8872560180b97037811773287d5a41a6282
adamrossbenton <adamrossbenton@gmail.com>
2022-03-16 22:17:23 -0700

    isogram python

--- python/isogram.py
+++ python/isogram.py
@@ -14,3 +14,15 @@ print(is_isogram("Dermatoglyphics"))
 print(is_isogram("isogram"))
 print(is_isogram("aba"))
 print(is_isogram("moOse"))
+
+# Way more elegant way to do this: use set(str)
+# Turns string into a set of chars found in string
+# Check if the string and its set are same length
+
+def is_iso_better(str):
+    return len(str) == len(set(str.lower()))
+
+print(is_iso_better("Dermatoglyphics"))
+print(is_iso_better("isogram"))
+print(is_iso_better("aba"))
+print(is_iso_better("moOse"))
