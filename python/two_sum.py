5110cb015eee3beace5a8990e9b7be66f623bc88
adamrossbenton <adamrossbenton@gmail.com>
2022-05-23 11:03:09 -0700

    leetcode

---
+++ python/two_sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                print(i)
+                print(j)
+                print(i+j)
+                if i+j == target:
+                    return [i,j]
+                else:
+                    continue
