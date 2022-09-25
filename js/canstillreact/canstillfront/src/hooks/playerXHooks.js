fc7ea7a43a34a010c0dba3b91b61db3d3b31ed5c
adamrossbenton <adamrossbenton@gmail.com>
2022-09-24 22:14:27 -0700

    figured out game logic

---
+++ js/canstillreact/canstillfront/src/hooks/playerXHooks.js
@@ -0,0 +1,11 @@
+import {useState} from "react"
+
+const PlayerXHooks = props => {
+    const [boxSelect, setBoxSelect] = useState(false)
+
+    const selectBox = box => {
+
+    }
+}
+
+export default PlayerXHooks
